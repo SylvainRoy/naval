e4dd523934410ba8cd30fa26dcc6b5514057b110
Sylvain Roy <sylvain.roy@m4x.org>
2022-01-16 18:16:39 +0100

    The islands now have a constant ground.

--- src/island.rs
+++ src/island.rs
@@ -27,13 +27,12 @@ fn islands_spawn(
     let h_tiles = (win_size.h / (2. * 16.)) as i32;
 
     // Create N islands
-    let mut drawn_tiles: HashMap<(i32, i32), bool> = HashMap::new();
-
+    let mut tiles: HashMap<(i32, i32), bool> = HashMap::new();
     for _island in 0..rng.gen_range((NUM_ISLANDS / 2)..NUM_ISLANDS) {
         // Place a new island at random
         let x = rng.gen_range(-w_tiles..=w_tiles);
         let y = rng.gen_range(-h_tiles..=h_tiles);
-        drawn_tiles.insert((x, y), true);
+        tiles.insert((x, y), true);
         // Grow the island
         for _tile in 0..rng.gen_range(0..SIZE_ISLANDS) {
             let (mut xx, mut yy) = (x, y);
@@ -49,7 +48,7 @@ fn islands_spawn(
                     break;
                 };
                 // If slot is empty, add a till
-                if let Vacant(e) = drawn_tiles.entry((xx, yy)) {
+                if let Vacant(e) = tiles.entry((xx, yy)) {
                     e.insert(true);
                     break;
                 };
@@ -58,18 +57,48 @@ fn islands_spawn(
     }
 
     // Spwan the ground tiles
-    for (tile_x, tile_y) in drawn_tiles.keys() {
+    for (tile_x, tile_y) in tiles.keys() {
+        // Determine sprite & orientation based on adjacent tiles
+        let right = tiles.contains_key(&(*tile_x+1, *tile_y));
+        let up = tiles.contains_key(&(*tile_x, *tile_y+1));
+        let left = tiles.contains_key(&(*tile_x-1, *tile_y));
+        let down = tiles.contains_key(&(*tile_x, *tile_y-1));
+        let (index, rotation) = match (right, up, left, down) {
+            // 0 adjacent
+            (false, false, false, false) => (sprite_materials.ground6_index, 0.),
+            // 1
+            (true, false, false, false) => (sprite_materials.ground5_index, -2.),
+            (false, true, false, false) => (sprite_materials.ground5_index, -1.),
+            (false, false, true, false) => (sprite_materials.ground5_index, 0.),
+            (false, false, false, true) => (sprite_materials.ground5_index, 1.),
+            // 2
+            (true, true, false, false) => (sprite_materials.ground3_index, -2.),
+            (false, true, true, false) => (sprite_materials.ground3_index, -1.),
+            (false, false, true, true) => (sprite_materials.ground3_index, 0.),
+            (true, false, false, true) => (sprite_materials.ground3_index, 1.),
+            
+            (true, false, true, false) => (sprite_materials.ground4_index, 0.),
+            (false, true, false, true) => (sprite_materials.ground4_index, 1.),
+            // 3
+            (true, true, true, false) => (sprite_materials.ground2_index, -1.),
+            (true, true, false, true) => (sprite_materials.ground2_index, 2.),
+            (true, false, true, true) => (sprite_materials.ground2_index, 1.),
+            (false, true, true, true) => (sprite_materials.ground2_index, 0.),
+            // 4
+            (true, true, true, true) => (sprite_materials.ground1_index, 0.),
+        };
+        // Spawn the ground tile
         commands
             .spawn_bundle(SpriteSheetBundle {
                 texture_atlas: sprite_materials.texture.clone(),
-                sprite: TextureAtlasSprite::new(sprite_materials.ground6_index),
+                sprite: TextureAtlasSprite::new(index),
                 transform: Transform {
                     translation: Vec3::new(
                         16. * (*tile_x as f32),
                         16. * (*tile_y as f32),
                         GROUND_Z,
                     ),
-                    // rotation: Quat::from_axis_angle(Vec3::new(0., 0., 1.), BOAT_INIT_ANGLE),
+                    rotation: Quat::from_axis_angle(Vec3::new(0., 0., 1.), rotation * std::f32::consts::PI / 2.),
                     // scale: Vec3::splat(1.0),
                     ..Default::default()
                 },
